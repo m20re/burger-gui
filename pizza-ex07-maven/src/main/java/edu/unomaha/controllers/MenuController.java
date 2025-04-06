f181c6c6cf639aca92f33cc5893ffc3eee9b9309
m20re <marvinorellana@unomaha.edu>
2025-04-06 16:45:12 -0500

    Added receipt menu with very simple error handling.

--- pizza-ex07-maven/src/main/java/edu/unomaha/controllers/MenuController.java
+++ pizza-ex07-maven/src/main/java/edu/unomaha/controllers/MenuController.java
@@ -1,10 +1,15 @@
 package edu.unomaha.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import edu.unomaha.burger.Burger;
+import edu.unomaha.burger.BurgerManager;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -32,20 +37,50 @@ public class MenuController {
         stage.show();
     }
 
-    public void switchToPizza(ActionEvent event) throws IOException {
-        /* Load the BurgerMenu */
-        root = FXMLLoader.load(getClass().getResource("/edu/unomaha/views/" + "PizzaMenu" + ".fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+    public void switchToReceipt(ActionEvent event) throws IOException {
 
-        /* Store and load the css stylesheet */
+        /* Ensures a receipt can't be printed out if nothing was added */
+        List<Burger> burgers = BurgerManager.getInstance().getBurgers();
+        if (burgers.isEmpty()) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("No Burgers");
+            alert.setHeaderText(null);
+            alert.setContentText("No burgers have been added.");
+            alert.showAndWait();
+            /* Return early */
+            return;
+        }
+
+        Parent root = FXMLLoader.load(getClass().getResource("/edu/unomaha/views/Receipt.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        
         String css = this.getClass().getResource("/edu/unomaha/views/styles.css").toExternalForm();
         scene.getStylesheets().add(css);
-
+        
         stage.setScene(scene);
-        stage.setTitle("Pizza Menu");
+        stage.setTitle("Receipt");
         stage.show();
     }
+    
+
+    /* Depreciated function */
+    // public void switchToPizza(ActionEvent event) throws IOException {
+    //     /* Load the BurgerMenu */
+    //     root = FXMLLoader.load(getClass().getResource("/edu/unomaha/views/" + "PizzaMenu" + ".fxml"));
+    //     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    //     scene = new Scene(root);
+
+    //     /* Store and load the css stylesheet */
+    //     String css = this.getClass().getResource("/edu/unomaha/views/styles.css").toExternalForm();
+    //     scene.getStylesheets().add(css);
+
+    //     stage.setScene(scene);
+    //     stage.setTitle("Pizza Menu");
+    //     stage.show();
+    // }
+
+
     
 
 
---
+++ pizza-ex07-maven/src/main/java/edu/unomaha/controllers/ReceiptController.java
@@ -0,0 +1,36 @@
+package edu.unomaha.controllers;
+
+import edu.unomaha.burger.Burger;
+import edu.unomaha.burger.BurgerManager;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ReceiptController implements Initializable {
+
+    @FXML
+    private TextArea receiptTextArea;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        List<Burger> burgers = BurgerManager.getInstance().getBurgers();
+        StringBuilder receipt = new StringBuilder();
+
+        if (burgers.isEmpty()) {
+            receipt.append("No burgers created yet.");
+        } else {
+            for (Burger burger : burgers) {
+                receipt.append(burger.toNiceString())
+                       .append("\nPrice: $")
+                       .append(String.format("%.2f", burger.getPrice()))
+                       .append("\n\n");
+            }
+        }
+        receiptTextArea.setText(receipt.toString());
+    }
+}

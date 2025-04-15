7db47c36fb2be0b72acb28121487990f1daab82a
m20re <marvinorellana@unomaha.edu>
2025-04-14 22:24:12 -0500

    Added extra tests.

--- pizza-ex07-maven/src/test/java/edu/unomaha/TestBurger.java
+++ pizza-ex07-maven/src/test/java/edu/unomaha/TestBurger.java
@@ -15,6 +15,7 @@ import edu.unomaha.burger.cheese.AmericanCheese;
 import edu.unomaha.burger.cheese.BurgerCheese;
 import edu.unomaha.burger.garnishes.BaconGarnish;
 import edu.unomaha.burger.garnishes.BurgerGarnish;
+import edu.unomaha.burger.garnishes.OnionringGarnish;
 import edu.unomaha.burger.meat.BurgerMeat;
 import edu.unomaha.burger.meat.ChickenMeat;
 import edu.unomaha.pizza.Pizza;
@@ -23,6 +24,8 @@ import edu.unomaha.pizza.crust.ThickCrust;
 import edu.unomaha.pizza.sauce.PizzaSauce;
 import edu.unomaha.pizza.sauce.TomatoSauce;
 import edu.unomaha.pizza.topping.MozzarellaTopping;
+import edu.unomaha.pizza.topping.PepperTopping;
+import edu.unomaha.pizza.topping.PepperoniTopping;
 import edu.unomaha.pizza.topping.PizzaTopping;
 
 public class TestBurger {
@@ -39,6 +42,10 @@ public class TestBurger {
 
     @BeforeEach
     public void setUp() {
+        Burger burg = new Burger();
+        initBurgerComponents();
+
+        
 
     }
     
@@ -51,16 +58,16 @@ public class TestBurger {
 
         /* Checks price upon each added component */
         burg.addComponent(bun);
-        assertEquals(0.50, burg.getPrice());
+        assertEquals(0.50, burg.getPrice(), 0.0001);
 
         burg.addComponent(cheese);
-        assertEquals(0.75, burg.getPrice());
+        assertEquals(0.75, burg.getPrice(), 0.0001);
 
         burg.addComponent(meat);
-        assertEquals(3.50, burg.getPrice());
+        assertEquals(3.50, burg.getPrice(), 0.0001);
 
         burg.addComponent(garnish1);
-        assertEquals(3.70, burg.getPrice());
+        assertEquals(3.70, burg.getPrice(), 0.0001);
     
         /* Adds a burger to the particular instance, which mirrors GUI interaction. */
         BurgerManager.getInstance().addBurger(burg);
@@ -78,16 +85,56 @@ public class TestBurger {
 
         /* Checks price upon each added component */
         pizza.addComponent(crust);
-        assertEquals(1.20, pizza.getPrice());
+        assertEquals(1.20, pizza.getPrice(), 0.0001);
 
         pizza.addComponent(sauce);
-        assertEquals(1.50, pizza.getPrice());
+        assertEquals(1.50, pizza.getPrice(), 0.0001);
 
         pizza.addComponent(topping);
-        assertEquals(1.70, pizza.getPrice());
+        assertEquals(1.70, pizza.getPrice(), 0.0001);
 
         return;
     }
+
+    @Test
+    public void testBurgerAssemble() {
+        Burger burg = new Burger();
+        initBurgerComponents();
+
+        /* Initializes a pizza */
+        burg.addComponent(bun);
+        burg.addComponent(cheese);
+        burg.addComponent(meat);
+        burg.addComponent(garnish1);
+        /* Ensures the price is correct */
+        assertEquals(3.7, burg.getPrice(), 0.0001);
+
+        /* Initialize a garnish and add it */
+        BurgerGarnish newGarnish = new OnionringGarnish();
+        burg.addTopping(newGarnish);
+        assertEquals(3.9, burg.getPrice(), 0.0001);
+        
+    }
+
+    @Test
+    public void testPizzaAssemble() {
+        Pizza pizza = new Pizza();
+        initPizzaComponents();
+
+        /* Initializes a pizza */
+        pizza.addComponent(crust);
+        pizza.addComponent(sauce);
+        pizza.addComponent(topping);
+        /* Ensures the price is correct */
+        assertEquals(1.70, pizza.getPrice(), 0.0001);
+
+        /* Initialize a topping and add it */
+        PizzaTopping newTopping = new PepperoniTopping();
+        pizza.addTopping(newTopping);
+        assertEquals(2.30, pizza.getPrice(), 0.0001);
+    }
+
+
 
 
     private void initBurgerComponents() {
